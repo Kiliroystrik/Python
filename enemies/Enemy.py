535f8b6a3d708204223294120472810e43f65e03
Kiliroy <borges.mathieu@gmail.com>
2022-01-20 17:20:01 +0100

    added xp bar and health bar for enemy

--- enemies/Enemy.py
+++ enemies/Enemy.py
@@ -1,17 +1,31 @@
 from random import random
+import pygame
+pygame.init()
+class Enemy(pygame.sprite.Sprite):
 
-
-class Enemy:
-
-    def __init__(self):
-        self.classe = "Brigand"
-        self.name = "Kaizer"
-        self.strenght = 100
+    def __init__(self, classe, name, surname):
+        super().__init__()
+        self.classe = classe
+        self.name = name
+        self.surname = surname
+        self.strenght = 10
         self.stamina = 10
-        self.life = 50
+        self.life = 100
+        self.maxLife = 100
         self.level = 1
         self.attack = self.strenght / 3
         self.expDrop = round((random() * 100),2)
+
+        self.image = pygame.transform.flip((pygame.image.load(f'assets/rpgDungeon/frames/{self.name}_idle_anim_f0.png')), True, False)
+        self.resizedImage = pygame.transform.scale(self.image, ((self.image.get_width() * 5), (self.image.get_height() *5)))
+        self.rect = self.resizedImage.get_rect()
+        self.rect.x = 500
+        self.rect.y = 140
+
+    def attackAnimation(self):
+        self.image = pygame.image.load(f'assets/rpgDungeon/frames/{self.name}_hit_anim_f0.png')
+
+
 
     def hit(self):
      hit = self.strenght/3
@@ -22,12 +36,21 @@ class Enemy:
         else: 
             return False
 
-# player1 = Player('Mage', 'Kiliroy', 'Striker')
+    def healthBar(self, Surface):
 
-"""
----TEST---
-print(player1.name)
-print(player1.classe)
-print(player1.surname)
-"""
+        # HEALTH BAR POSITION
+        LIFE_BAR_POSITION = [self.rect.x , (self.rect.y + 20), self.life, 10]
+        # HEALTH BAR COLOR
+        LIFE_BAR_COLOR = (111, 210, 46)
+
+        # MAX HEALTH BAR POSITION
+        MAX_LIFE_BAR_POSITION = [self.rect.x , (self.rect.y + 20), self.maxLife, 10]
+        # MAX HEALTH BAR COLOR
+        MAX_LIFE_BAR_COLOR = (15, 15, 15)
+
+        pygame.draw.rect(Surface, MAX_LIFE_BAR_COLOR, MAX_LIFE_BAR_POSITION)
+        pygame.draw.rect(Surface, LIFE_BAR_COLOR, LIFE_BAR_POSITION)
+
+    def drawDead(self):
+        self.image = pygame.transform.flip((pygame.image.load(f'assets/rpgDungeon/frames/{self.name}_idle_anim_f0.png')), True, True)
 
